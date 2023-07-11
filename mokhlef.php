863e4547053413910283c97fafeadadb1e6848df
makiomar <mo7amed.maki@gmail.com>
2023-07-11 10:49:18 +0300

    Version revert

--- mokhlef.php
+++ mokhlef.php
@@ -9,7 +9,7 @@
  * that starts the plugin.
  *
  * @link              https://https://github.com/MakiOmar
- * @since             1.0.01
+ * @since             1.0.0
  * @package           Mokhlef
  *
  * @wordpress-plugin
